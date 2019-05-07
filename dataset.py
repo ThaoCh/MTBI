e74ba6ebaba804bb620356fe24c5bde584cc1649
ThaoCh <thc18001@gmail.com>
2019-05-07 14:49:24 -0400

    Add Morph Function

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 from torch.utils.data import Dataset
 from scipy.ndimage import affine_transform
 from datautility import *
+from PIL import Image
 
 def toTensor (sample):
 	'''
@@ -106,6 +107,119 @@ def downSampleFun(img, level, order):
 		for chan in np.arange(C):
 			imgout[chan] = affine_transform(img[chan], Matrix, output_shape=imgout[chan].shape, order=order)
 		return imgout
+
+'''
+Functions for 2D_image morphing.
+
+'''
+def quad_as_rect(quad):
+    if quad[0] != quad[2]: return False
+    if quad[1] != quad[7]: return False
+    if quad[4] != quad[6]: return False
+    if quad[3] != quad[5]: return False
+    return True
+
+def quad_to_rect(quad):
+    assert(len(quad) == 8)
+    assert(quad_as_rect(quad))
+    return (quad[0], quad[1], quad[4], quad[3])
+
+def rect_to_quad(rect):
+    assert(len(rect) == 4)
+    return (rect[0], rect[1], rect[0], rect[3], rect[2], rect[3], rect[2], rect[1])
+
+def shape_to_rect(shape):
+    assert(len(shape) == 2)
+    return (0, 0, shape[0], shape[1])
+
+def griddify(rect, w_div, h_div):
+    w = rect[2] - rect[0]
+    h = rect[3] - rect[1]
+    x_step = w / float(w_div)
+    y_step = h / float(h_div)
+    y = rect[1]
+    grid_vertex_matrix = []
+    for _ in range(h_div + 1):
+        grid_vertex_matrix.append([])
+        x = rect[0]
+        for _ in range(w_div + 1):
+            grid_vertex_matrix[-1].append([int(x), int(y)])
+            x += x_step
+        y += y_step
+    grid = np.array(grid_vertex_matrix)
+    return grid
+
+def distort_grid(org_grid, max_shift):
+    new_grid = np.copy(org_grid)
+    x_min = np.min(new_grid[:, :, 0])
+    y_min = np.min(new_grid[:, :, 1])
+    x_max = np.max(new_grid[:, :, 0])
+    y_max = np.max(new_grid[:, :, 1])
+    new_grid += np.random.randint(- max_shift, max_shift + 1, new_grid.shape)
+    new_grid[:, :, 0] = np.maximum(x_min, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.maximum(y_min, new_grid[:, :, 1])
+    new_grid[:, :, 0] = np.minimum(x_max, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.minimum(y_max, new_grid[:, :, 1])
+    return new_grid
+
+def grid_to_mesh(src_grid, dst_grid):
+    assert(src_grid.shape == dst_grid.shape)
+    mesh = []
+    for i in range(src_grid.shape[0] - 1):
+        for j in range(src_grid.shape[1] - 1):
+            src_quad = [src_grid[i    , j    , 0], src_grid[i    , j    , 1],
+                        src_grid[i + 1, j    , 0], src_grid[i + 1, j    , 1],
+                        src_grid[i + 1, j + 1, 0], src_grid[i + 1, j + 1, 1],
+                        src_grid[i    , j + 1, 0], src_grid[i    , j + 1, 1]]
+            dst_quad = [dst_grid[i    , j    , 0], dst_grid[i    , j    , 1],
+                        dst_grid[i + 1, j    , 0], dst_grid[i + 1, j    , 1],
+                        dst_grid[i + 1, j + 1, 0], dst_grid[i + 1, j + 1, 1],
+                        dst_grid[i    , j + 1, 0], dst_grid[i    , j + 1, 1]]
+            dst_rect = quad_to_rect(dst_quad)
+            mesh.append([dst_rect, src_quad])
+    return mesh
+
+def xy_morph(imgArray, delta):
+	C,X,Y,Z = imgArray.shape
+	result = np.zeros(imgArray.shape)
+	for z in range(Z):
+		for c in range(C):
+			img = Image.fromarray(imgArray[c,:,:,z])
+			dst_grid  = griddify(shape_to_rect(img.size), 4, 4)
+			src_grid = distort_grid(dst_grid, delta)
+			mesh = grid_to_mesh(src_grid, dst_grid)
+			img = img.transform(img.size, Image.MESH, mesh)
+			result[c,:,:,z] = np.array(img)
+	return result
+
+'''
+Functions for 1D_morphing
+'''
+def distort_grid_1D(org_grid, max_shift):
+    new_grid = np.copy(org_grid)
+    # x_min = np.min(new_grid[:, :, 0])
+    y_min = np.min(new_grid[:, :, 1])
+    # x_max = np.max(new_grid[:, :, 0])
+    y_max = np.max(new_grid[:, :, 1])
+    new_grid[:, :, 1] += np.random.randint(- max_shift, max_shift + 1, new_grid[:, :, 1].shape)
+    # new_grid[:, :, 0] = np.maximum(x_min, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.maximum(y_min, new_grid[:, :, 1])
+    # new_grid[:, :, 0] = np.minimum(x_max, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.minimum(y_max, new_grid[:, :, 1])
+    return new_grid
+
+def z_morph(imgArray, delta):
+	C,X,Y,Z = imgArray.shape
+	result = np.zeros(imgArray.shape)
+	for x in range(X):
+		for c in range(C):
+			img = Image.fromarray(imgArray[c,x,:,:])
+			dst_grid  = griddify(shape_to_rect(img.size), 4, 4)
+			src_grid = distort_grid_1D(dst_grid, delta)
+			mesh = grid_to_mesh(src_grid, dst_grid)
+			img = img.transform(img.size, Image.MESH, mesh)
+			result[c,x,:,:] = np.array(img)
+	return result
 
 class downSample(object):
 	'''
